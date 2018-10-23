1c284e8e6439da13674906138e3ec375d029497c
grunam <tiabnamik@gmail.com>
2018-10-23 19:26:18 +0200

    V 1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+//import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/interval';
+
+//import { Subscription } from 'rxjs-compat/subscription';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  
+  //constructor() { }
+
+  secondes : number;
+  counterSubscription: Subscription;  
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
+  
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
+  
+  
+  /*
+  isAuth = false;
+  
+  
+  appareils: any[];
+  
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+}
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  
+  onEteindre() {
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+  }
+  */
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
+import { AppareilService } from './services/appareil.service';
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
+
+
+const appRoutes: Routes = [
+    { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+    { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
+    { path:'edit', canActivate: [AuthGuard], component: EditAppareilComponent },
+    { path: 'auth', component: AuthComponent },
+    { path: 'users', component: UserListComponent },
+    { path: 'new-user', component: NewUserComponent },
+    { path: '', canActivate: [AuthGuard], component: AppareilViewComponent },
+    { path: 'not-found', component: FourOhFourComponent },
+    { path: '**', redirectTo: 'not-found' }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent,
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AppareilService,
+    AuthService,
+    AuthGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
